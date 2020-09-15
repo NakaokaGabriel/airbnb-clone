4bc207fd250f2c8013699b8dbcf02c9d5f6fba25
nakaokagabriel <gabrielnakaoka_99@hotmail.com>
2020-09-15 20:08:56 -0300

    feature: mobile styles introduction

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,5 +1,42 @@
 import styled from 'styled-components';
 
-export const Introduce = styled.div``;
+import background from '../../assets/HERO_BG.jpg';
 
-export const Background = styled.div``;
+export const Introduce = styled.div`
+  margin: 10px 0;
+`;
+
+export const Background = styled.div`
+  border-radius: 4px;
+  background: url(${background}) no-repeat;
+  background-size: cover;
+  padding: 20px 10px;
+
+  h1,
+  p,
+  button {
+    color: #ffffff;
+    max-width: 300px;
+  }
+
+  h1 {
+    font-size: 28px;
+    font-weight: 300;
+  }
+
+  p {
+    margin: 20px 0;
+    font-size: 14px;
+  }
+
+  button {
+    background: none;
+    border: none;
+    font-size: 14px;
+    font-weight: bold;
+
+    svg {
+      vertical-align: middle;
+    }
+  }
+`;
