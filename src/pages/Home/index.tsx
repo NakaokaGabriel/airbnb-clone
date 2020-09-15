30b909918134e5d773aeb4a8303d769275fcb573
nakaokagabriel <gabrielnakaoka_99@hotmail.com>
2020-09-14 22:23:41 -0300

    feature: introduction structure

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,13 +5,24 @@ import Container from '../../components/Container';
 import Navigation from '../../components/Navigation';
 import Header from '../../components/Header';
 
+import { Introduce, Background } from './styles';
+
 const Home: React.FC = () => {
   return (
     <>
       <Navigation />
       <Container>
         <Header />
-        <h1>hello world</h1>
+
+        <Introduce>
+          <Background>
+            <h1>Um mundo inteiro perto de você</h1>
+            <p>
+              Faça uma viagem diferente e descubra as maravilhas perto de você.
+            </p>
+            <button type="button">Explore os arredores</button>
+          </Background>
+        </Introduce>
       </Container>
     </>
   );
