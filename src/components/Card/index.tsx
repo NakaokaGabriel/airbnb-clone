6a578fbe1e2e968f94d99cf17b2266ef7e3fb77b
nakaokagabriel <gabrielnakaoka_99@hotmail.com>
2020-09-20 00:26:31 -0300

    feature: create styles to cards

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -4,10 +4,15 @@ import { Container } from './styles';
 
 export interface CardProps {
   type?: 'mini' | 'medium' | 'large';
+  to: string;
 }
 
-const Card: React.FC<CardProps> = ({ children, type }) => {
-  return <Container type={type}>{children}</Container>;
+const Card: React.FC<CardProps> = ({ children, type, to }) => {
+  return (
+    <Container to={to} type={type}>
+      {children}
+    </Container>
+  );
 };
 
 export default Card;
--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -1,5 +1,112 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { Link } from 'react-router-dom';
+
+import { colors } from '../../styles/colors';
 
 import { CardProps } from '.';
 
-export const Container = styled.div<CardProps>``;
+export const Container = styled(Link)<CardProps>`
+  margin-top: 20px;
+  display: block;
+  text-decoration: none;
+
+  ${(props) => props.type === 'mini'
+    && css`
+      max-width: 200px;
+
+      > img {
+        height: 240px;
+        width: 100%;
+        border-radius: 4px;
+      }
+
+      > div {
+        margin-top: 5px;
+
+        span {
+          color: ${colors.middleGray};
+          font-weight: bold;
+          font-size: 12px;
+          text-transform: uppercase;
+        }
+
+        h4 {
+          margin: 5px 0;
+          color: ${colors.darkGrey};
+        }
+
+        p {
+          color: ${colors.darkGrey};
+          font-size: 16px;
+        }
+      }
+    `}
+
+  ${(props) => props.type === 'medium'
+    && css`
+      max-width: 320px;
+
+      > img {
+        height: 240px;
+        width: 100%;
+        border-radius: 4px;
+      }
+
+      .content {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        div {
+          margin: 10px 0;
+
+          strong {
+            border: 1px solid ${colors.darkGrey};
+            border-radius: 4px;
+            color: ${colors.darkGrey};
+            text-transform: uppercase;
+            padding: 5px;
+            display: inline-block;
+            font-size: 10px;
+          }
+
+          span {
+            font-size: 12px;
+            color: ${colors.middleGray};
+            margin-left: 10px;
+          }
+        }
+
+        .stars {
+          font-size: 14px;
+          color: ${colors.darkGrey};
+          font-weight: bold;
+        }
+      }
+
+      > p {
+        color: ${colors.darkGrey};
+      }
+    `}
+
+  ${(props) => props.type === 'large'
+    && css`
+      max-width: 400px;
+
+      > img {
+        height: 240px;
+        width: 100%;
+        border-radius: 4px;
+      }
+
+      h5 {
+        color: ${colors.purple};
+        font-size: 14px;
+        margin: 5px 0;
+      }
+
+      p {
+        color: ${colors.darkGrey};
+      }
+    `}
+`;
