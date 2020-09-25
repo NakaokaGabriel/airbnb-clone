9e9cf3d1b9a7c9c4a5fe90b3410715358e9f0e9e
nakaokagabriel <gabrielnakaoka_99@hotmail.com>
2020-09-24 22:12:28 -0300

    feature: create home experience component

--- src/pages/Home/Experience/styles.ts
+++ src/pages/Home/Experience/styles.ts
@@ -1,3 +1,18 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+import { colors } from '../../../styles/colors';
+
+export const Container = styled.div`
+  margin: 40px 0;
+
+  p {
+    font-size: 12px;
+    color: ${colors.middleGray};
+    margin-top: 10px;
+
+    @media screen and (min-width: 768px) {
+      font-size: 16px;
+      max-width: 500px;
+    }
+  }
+`;
