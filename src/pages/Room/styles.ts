dfc1ed47a878f881b30b0a874a637f8bc0f19c1d
nakaokagabriel <gabrielnakaoka_99@hotmail.com>
2020-10-26 22:16:27 -0300

    feature: create all sctructure in room page

--- src/pages/Room/styles.ts
+++ src/pages/Room/styles.ts
@@ -1,3 +1,15 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Content = styled.div``;
+
+export const Title = styled.div``;
+
+export const RowContent = styled.div``;
+
+export const MainContent = styled.div``;
+
+export const Location = styled.div``;
+
+export const Host = styled.div``;
+
+export const Description = styled.div``;
